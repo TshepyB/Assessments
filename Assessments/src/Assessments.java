af1fd4cccd7332e2c8b053c2edf6067536d33f63
TshepyB <144884304+TshepyB@users.noreply.github.com>
2024-01-26 18:14:30 +0200

    Add files via upload

---
+++ Assessments/src/Assessments.java
@@ -0,0 +1,19 @@
+public class Assessments{
+
+            
+    public static int addNumbers(int a, int b, int c, int d) {
+        
+        
+        int sum = a + b + c + d;
+        
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        
+        int result = addNumbers(3, 5, 6,9);
+        
+        System.out.println("The sum is: " + result);
+    }
+}
+
